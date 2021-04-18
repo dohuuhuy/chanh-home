06be4727489e2e74672b036e62f1773f100198da
dohuuhuy <dohuuhuy2@gmail.com>
2021-04-18 15:23:15 +0700

    thắc mắc

--- src/assets/js/quick-website.js
+++ src/assets/js/quick-website.js
@@ -2,350 +2,372 @@
 // Popover
 //
 
-'use strict';
+"use strict";
 
-var Popover = (function() {
+var Popover = (function () {
+  // Variables
 
-	// Variables
+  var $popover = $('[data-toggle="popover"]');
 
-	var $popover = $('[data-toggle="popover"]');
+  // Methods
 
+  function init($this) {
+    var popoverClass = "";
 
-	// Methods
+    if ($this.data("color")) {
+      popoverClass = " popover-" + $this.data("color");
+    }
 
-	function init($this) {
-		var popoverClass = '';
+    var options = {
+      trigger: "focus",
+      template:
+        '<div class="popover' +
+        popoverClass +
+        '" role="tooltip"><div class="arrow"></div><h3 class="popover-header"></h3><div class="popover-body"></div></div>',
+    };
 
-		if ($this.data('color')) {
-			popoverClass = ' popover-' + $this.data('color');
-		}
+    $this.popover(options);
+  }
 
-		var options = {
-			trigger: 'focus',
-			template: '<div class="popover' + popoverClass + '" role="tooltip"><div class="arrow"></div><h3 class="popover-header"></h3><div class="popover-body"></div></div>'
-		};
+  // Events
 
-		$this.popover(options);
-	}
-
-
-	// Events
-
-	if ($popover.length) {
-		$popover.each(function() {
-			init($(this));
-		});
-	}
-
+  if ($popover.length) {
+    $popover.each(function () {
+      init($(this));
+    });
+  }
 })();
 
 //
 // Tooltip
 //
 
-'use strict';
+("use strict");
 
-var Tooltip = (function() {
+var Tooltip = (function () {
+  // Variables
 
-	// Variables
+  var $tooltip = $('[data-toggle="tooltip"]');
 
-	var $tooltip = $('[data-toggle="tooltip"]');
+  // Methods
 
+  function init() {
+    $tooltip.tooltip();
+  }
 
-	// Methods
+  // Events
 
-	function init() {
-		$tooltip.tooltip();
-	}
-
-
-	// Events
-
-	if ($tooltip.length) {
-		init();
-	}
-
+  if ($tooltip.length) {
+    init();
+  }
 })();
 
-var CopyType = (function() {
+var CopyType = (function () {
+  // Variables
 
-	// Variables
+  var $element = ".btn-type-clipboard",
+    $btn = $($element);
 
-	var $element = '.btn-type-clipboard',
-		$btn = $($element);
+  // Methods
 
+  function init($this) {
+    $this.tooltip().on("mouseleave", function () {
+      // Explicitly hide tooltip, since after clicking it remains
+      // focused (as it's a button), so tooltip would otherwise
+      // remain visible until focus is moved away
+      $this.tooltip("hide");
+    });
 
-	// Methods
+    var clipboard = new ClipboardJS($element);
 
-	function init($this) {
-		$this.tooltip().on('mouseleave', function() {
-			// Explicitly hide tooltip, since after clicking it remains
-			// focused (as it's a button), so tooltip would otherwise
-			// remain visible until focus is moved away
-			$this.tooltip('hide');
-		});
+    clipboard.on("success", function (e) {
+      $(e.trigger)
+        .attr("title", "Copied!")
+        .tooltip("_fixTitle")
+        .tooltip("show")
+        .attr("title", "Copy to clipboard")
+        .tooltip("_fixTitle");
 
-		var clipboard = new ClipboardJS($element);
+      e.clearSelection();
+    });
+  }
 
-		clipboard.on('success', function(e) {
-			$(e.trigger)
-				.attr('title', 'Copied!')
-				.tooltip('_fixTitle')
-				.tooltip('show')
-				.attr('title', 'Copy to clipboard')
-				.tooltip('_fixTitle')
-
-			e.clearSelection()
-		});
-	}
-
-
-	// Events
-	if ($btn.length) {
-		init($btn);
-	}
-
+  // Events
+  if ($btn.length) {
+    init($btn);
+  }
 })();
 
 //
 // Forms
 //
 
-'use strict';
-
+("use strict");
 
 //
 // Form control
 //
 
-var FormControl = (function() {
+var FormControl = (function () {
+  // Variables
 
-	// Variables
+  var $input = $(".form-control"),
+    $indeterminateCheckbox = $('[data-toggle="indeterminate"]');
 
-	var $input = $('.form-control'),
-		$indeterminateCheckbox = $('[data-toggle="indeterminate"]');
+  // Methods
 
+  function init($this) {
+    $this
+      .on("focus blur", function (e) {
+        $(this)
+          .parents(".form-group")
+          .toggleClass("focused", e.type === "focus");
+      })
+      .trigger("blur");
+  }
 
-	// Methods
+  // Events
 
-	function init($this) {
-		$this.on('focus blur', function(e) {
-        	$(this).parents('.form-group').toggleClass('focused', (e.type === 'focus'));
-    	}).trigger('blur');
-	}
+  if ($input.length) {
+    init($input);
+  }
 
-
-	// Events
-
-	if ($input.length) {
-		init($input);
-	}
-
-	// Add indeterminate state to a checkbox
-	if($indeterminateCheckbox.length) {
-		$indeterminateCheckbox.each(function() {
-			$(this).prop('indeterminate', true)
-		})
-	}
-
+  // Add indeterminate state to a checkbox
+  if ($indeterminateCheckbox.length) {
+    $indeterminateCheckbox.each(function () {
+      $(this).prop("indeterminate", true);
+    });
+  }
 })();
-
 
 //
 // Custom input file
 //
 
-var CustomInputFile = (function() {
+var CustomInputFile = (function () {
+  // Variables
 
-	// Variables
+  var $customInputFile = $(".custom-input-file");
 
-	var $customInputFile = $('.custom-input-file');
+  // Methods
 
+  function change($input, $this, $e) {
+    var fileName,
+      $label = $input.next("label"),
+      labelVal = $label.html();
 
-	// Methods
+    if ($this && $this.files.length > 1) {
+      fileName = ($this.getAttribute("data-multiple-caption") || "").replace(
+        "{count}",
+        $this.files.length
+      );
+    } else if ($e.target.value) {
+      fileName = $e.target.value.split("\\").pop();
+    }
 
-	function change($input, $this, $e) {
-		var fileName,
-			$label = $input.next('label'),
-			labelVal = $label.html();
+    if (fileName) {
+      $label.find("span").html(fileName);
+    } else {
+      $label.html(labelVal);
+    }
+  }
 
-		if ($this && $this.files.length > 1) {
-			fileName = ($this.getAttribute('data-multiple-caption') || '').replace('{count}', $this.files.length);
-		}
-		else if ($e.target.value) {
-			fileName = $e.target.value.split('\\').pop();
-		}
+  function focus($input) {
+    $input.addClass("has-focus");
+  }
 
-		if (fileName) {
-			$label.find('span').html(fileName);
-		}
-		else {
-			$label.html(labelVal);
-		}
-	}
+  function blur($input) {
+    $input.removeClass("has-focus");
+  }
 
-	function focus($input) {
-		$input.addClass('has-focus');
-	}
+  // Events
 
-	function blur($input) {
-		$input.removeClass('has-focus');
-	}
+  if ($customInputFile.length) {
+    $customInputFile.each(function () {
+      var $input = $(this);
 
+      $input.on("change", function (e) {
+        var $this = this,
+          $e = e;
 
-	// Events
+        change($input, $this, $e);
+      });
 
-	if ($customInputFile.length) {
-		$customInputFile.each(function() {
-			var $input = $(this);
-
-			$input.on('change', function(e) {
-				var $this = this,
-					$e = e;
-
-				change($input, $this, $e);
-	        });
-
-	        // Firefox bug fix
-	        $input.on('focus', function() {
-	            focus($input);
-	        })
-	        .on('blur', function() {
-	            blur($input);
-	        });
-		});
-	}
+      // Firefox bug fix
+      $input
+        .on("focus", function () {
+          focus($input);
+        })
+        .on("blur", function () {
+          blur($input);
+        });
+    });
+  }
 })();
 
 //
 // Sticky Navbar
 //
 
-var NavbarSticky = (function() {
+var NavbarSticky = (function () {
+  // Variables
 
-	// Variables
+  var $nav = $(".navbar-sticky"),
+    navOffsetTop = 0,
+    scrolling = false;
 
-	var $nav = $('.navbar-sticky'),
-        navOffsetTop = 0,
-		scrolling = false;
+  // Methods
 
+  function init($this) {
+    // our current vertical position from the top
+    var scrollTop = $(window).scrollTop(),
+      navHeight = $this.outerHeight();
 
-	// Methods
+    if (scrollTop > navOffsetTop + 200) {
+      $this.addClass("sticky");
+      $("body").css("padding-top", navHeight + "px");
+    } else {
+      $this.removeClass("sticky");
+      $("body").css("padding-top", "0");
+    }
+  }
 
-	function init($this) {
+  // Events
 
-		// our current vertical position from the top
-		var scrollTop = $(window).scrollTop(),
-			navHeight = $this.outerHeight();
+  if ($nav.length) {
+    navOffsetTop = $nav.offset().top;
 
-		if (scrollTop > (navOffsetTop + 200)) {
-			$this.addClass('sticky');
-			$("body").css("padding-top", navHeight + "px");
-		} else {
-			$this.removeClass('sticky');
-			$("body").css("padding-top", "0");
-		}
-	}
+    $(window).on({
+      scroll: function () {
+        scrolling = true;
 
+        setInterval(function () {
+          if (scrolling) {
+            scrolling = false;
 
-	// Events
-
-	if ($nav.length) {
-
-		navOffsetTop = $nav.offset().top;
-		
-		$(window).on({
-			'scroll': function() {
-				scrolling = true;
-
-				setInterval(function() {
-					if (scrolling) {
-						scrolling = false;
-
-						// Sticky navbar init
-                        init($nav);
-					}
-				}, 250);
-			}
-		})
-	}
+            // Sticky navbar init
+            init($nav);
+          }
+        }, 250);
+      },
+    });
+  }
 })();
 
 //
 // Highlight.js
 //
 
-'use strict';
+("use strict");
 
-var Highlight = (function() {
+var Highlight = (function () {
+  //
+  // Variables
+  //
 
-	//
-	// Variables
-	//
+  var $highlight = $(".highlight");
 
-	var $highlight = $('.highlight');
+  //
+  // Methods
+  //
 
+  function init(i, block) {
+    // Insert the copy button inside the highlight block
+    var btnHtml =
+      '<button class="action-item btn-clipboard" title="Copy to clipboard"><i data-feather="copy"></i></button>';
+    $(block).before(btnHtml);
+    $(".btn-clipboard")
+      .tooltip()
+      .on("mouseleave", function () {
+        // Explicitly hide tooltip, since after clicking it remains
+        // focused (as it's a button), so tooltip would otherwise
+        // remain visible until focus is moved away
+        $(this).tooltip("hide");
+      });
 
-	//
-	// Methods
-	//
+    // Component code copy/paste
+    var clipboard = new ClipboardJS(".btn-clipboard", {
+      target: function (trigger) {
+        return trigger.nextElementSibling;
+      },
+    });
 
-	function init(i, block) {
-		// Insert the copy button inside the highlight block
-		var btnHtml = '<button class="action-item btn-clipboard" title="Copy to clipboard"><i data-feather="copy"></i></button>'
-		$(block).before(btnHtml)
-		$('.btn-clipboard')
-			.tooltip()
-			.on('mouseleave', function() {
-				// Explicitly hide tooltip, since after clicking it remains
-				// focused (as it's a button), so tooltip would otherwise
-				// remain visible until focus is moved away
-				$(this).tooltip('hide');
-			});
+    clipboard.on("success", function (e) {
+      $(e.trigger)
+        .attr("title", "Copied!")
+        .tooltip("_fixTitle")
+        .tooltip("show")
+        .attr("title", "Copy to clipboard")
+        .tooltip("_fixTitle");
 
-		// Component code copy/paste
-		var clipboard = new ClipboardJS('.btn-clipboard', {
-			target: function(trigger) {
-				return trigger.nextElementSibling
-			}
-		})
+      e.clearSelection();
+    });
 
-		clipboard.on('success', function(e) {
-			$(e.trigger)
-				.attr('title', 'Copied!')
-				.tooltip('_fixTitle')
-				.tooltip('show')
-				.attr('title', 'Copy to clipboard')
-				.tooltip('_fixTitle')
+    clipboard.on("error", function (e) {
+      var modifierKey = /Mac/i.test(navigator.userAgent) ? "\u2318" : "Ctrl-";
+      var fallbackMsg = "Press " + modifierKey + "C to copy";
 
-			e.clearSelection()
-		})
+      $(e.trigger)
+        .attr("title", fallbackMsg)
+        .tooltip("_fixTitle")
+        .tooltip("show")
+        .attr("title", "Copy to clipboard")
+        .tooltip("_fixTitle");
+    });
 
-		clipboard.on('error', function(e) {
-			var modifierKey = /Mac/i.test(navigator.userAgent) ? '\u2318' : 'Ctrl-'
-			var fallbackMsg = 'Press ' + modifierKey + 'C to copy'
+    // Initialize highlight.js plugin
+    hljs.highlightBlock(block);
+  }
 
-			$(e.trigger)
-				.attr('title', fallbackMsg)
-				.tooltip('_fixTitle')
-				.tooltip('show')
-				.attr('title', 'Copy to clipboard')
-				.tooltip('_fixTitle')
-		})
+  //
+  // Events
+  //
 
-		// Initialize highlight.js plugin
-		hljs.highlightBlock(block);
-	}
-
-
-	//
-	// Events
-	//
-
-	$highlight.each(function(i, block) {
-		init(i, block);
-	});
-
+  $highlight.each(function (i, block) {
+    init(i, block);
+  });
 })();
 
 //# sourceMappingURL=quick-website.js.map
+
+(function ($) {
+  "use strict"; // Start of use strict
+
+  // Prevent the content wrapper from scrolling when the fixed side navigation hovered over
+  $("body.fixed-nav .sidebar").on(
+    "mousewheel DOMMouseScroll wheel",
+    function (e) {
+      if ($(window).width() > 768) {
+        var e0 = e.originalEvent,
+          delta = e0.wheelDelta || -e0.detail;
+        this.scrollTop += (delta < 0 ? 1 : -1) * 30;
+        e.preventDefault();
+      }
+    }
+  );
+
+  // Scroll to top button appear
+  $(document).on("scroll", function () {
+    var scrollDistance = $(this).scrollTop();
+    if (scrollDistance > 100) {
+      $(".scroll-to-top").fadeIn();
+    } else {
+      $(".scroll-to-top").fadeOut();
+    }
+  });
+
+  // Smooth scrolling using jQuery easing
+  $(document).on("click", "a.scroll-to-top", function (e) {
+    var $anchor = $(this);
+    $("html, body")
+      .stop()
+      .animate(
+        {
+          scrollTop: $($anchor.attr("href")).offset().top,
+        },
+        1000,
+        "easeInOutExpo"
+      );
+    e.preventDefault();
+  });
+})(jQuery); // End of use strict
